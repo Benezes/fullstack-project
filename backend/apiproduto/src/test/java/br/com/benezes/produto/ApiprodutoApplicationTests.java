8934ac55908aba31822bc906d7b2d6da2049256d
mzn <mzn@Ubuntu-mnz.myguest.virtualbox.org>
2023-02-01 20:01:33 -0300

    create: initial project

---
+++ backend/apiproduto/src/test/java/br/com/benezes/produto/ApiprodutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.benezes.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiprodutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
